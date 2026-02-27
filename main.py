a1aac26d79338bf7523c901a598812f4c6607569
yashG0 <yashgaurkar0@gmail.com>
2026-02-27 08:11:26 +0530

    added frontend url in cors permission

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
+        "https://yashg0.github.io",
     ],
     allow_credentials=True,
     allow_methods=["*"],
